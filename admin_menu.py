f0309468acd679ec63b83ca423df9f8b1d497250
redmex77 <redmex@outlook.com>
2017-11-14 11:32:22 +0100

    added breaks to admin menu

--- admin_menu.py
+++ admin_menu.py
@@ -4,17 +4,24 @@ def mainMenu():
     print("1. Menu option 1")
     print("2. Menu option 2")
     print("3. Exit")
-    selection = int(input("Choose an option: "))
+    while True:
+      try:
+            selection = int(input("Choose an option: "))
 
-    if selection==1:
-        option1()
-    elif selection==2:
-        option2()
-    elif selection==3:
-        exit
-    else:
-        print("Invalid choice. Choose 1-3 ")
-        mainMenu()
+            if selection==1:
+                  option1()
+                  break
+            elif selection==2:
+                  option2()
+                  break
+            elif selection==3:
+                  break
+            else:
+                  print("Invalid choice. Choose 1-3")
+                  mainMenu()
+      except ValueError:
+            print("Invalid choice. Choose 1-3")
+    exit
 
 def option1():
     print("Option 1")
